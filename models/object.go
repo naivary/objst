2f7bbe742e0c9ff6eb33439b8900b14fb9e65ac0
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-06-17 18:12:17 +0200

    staging

--- models/object.go
+++ models/object.go
@@ -3,9 +3,9 @@ package models
 import "net/url"
 
 type Object struct {
-	ID string
-	Name  string
-	Owner string
+	ID      string
+	Name    string
+	Owner   string
 	Meta    url.Values
 	Payload []byte
 }
