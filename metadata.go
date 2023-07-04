9a5f2751c90d99c629a7788079de20e7d196cda2
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2023-07-04 13:30:01 +0200

    added metadata

--- metadata.go
+++ metadata.go
@@ -1,43 +1,60 @@
 package objst
 
-import "net/url"
+import (
+	"net/url"
+
+	"golang.org/x/exp/slices"
+)
 
 type MetaKey string
 
 const (
-	MetaKeyCreatedAt    MetaKey = "createdAt"
-	MetaKeyLastModified MetaKey = "lastModified"
-	MetaKeyContentType  MetaKey = "contentType"
+	MetaKeyCreatedAt   MetaKey = "createdAt"
+	MetaKeyContentType MetaKey = "contentType"
+	MetaKeyName        MetaKey = "name"
+	MetaKeyID          MetaKey = "id"
 )
 
-type Metadata map[string]string
+func (m MetaKey) String() string {
+	return string(m)
+}
+
+type Metadata struct {
+	data       map[MetaKey]string
+	systemKeys []MetaKey
+}
 
 func NewMetadata() Metadata {
-	return Metadata(map[string]string{})
+	return Metadata{
+		data:       make(map[MetaKey]string),
+		systemKeys: []MetaKey{MetaKeyID, MetaKeyCreatedAt},
+	}
 }
 
-func (m Metadata) Set(k string, v string) {
-	m[k] = v
+func (m Metadata) Set(k MetaKey, v string) {
+	if slices.Contains(m.systemKeys, k) {
+		return
+	}
+	m.data[k] = v
 }
 
-func (m Metadata) Has(k string) bool {
-	_, ok := m[k]
+func (m Metadata) Has(k MetaKey) bool {
+	_, ok := m.data[k]
 	return ok
 }
 
-func (m Metadata) Get(k string) string {
-	return m[k]
+func (m Metadata) Get(k MetaKey) string {
+	return m.data[k]
 }
 
-
-func (m Metadata) Del(k string) {
-	delete(m, k)
+func (m Metadata) Del(k MetaKey) {
+	delete(m.data, k)
 }
 
 func (m Metadata) Encode() string {
 	values := url.Values{}
-	for k, v := range m {
-		values.Set(k, v)
+	for k, v := range m.data {
+		values.Set(k.String(), v)
 	}
 	return values.Encode()
 }
